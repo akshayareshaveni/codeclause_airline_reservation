afb6c1588d784663634aafb32c146bc40724e4dc
akshayareshaveni <123267872+akshayareshaveni@users.noreply.github.com>
2023-05-28 00:41:54 +0530

    Add files via upload

---
+++ airline/passenger.java
@@ -0,0 +1,37 @@
+package airport.app;
+
+import java.util.Date;
+
+public class Passenger extends Person{
+     private int passportNumber;
+
+    public Passenger(int passportNumber, String name, Date dateOfBirth, String nationality) {
+        this.passportNumber = passportNumber;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+    }
+
+    public int getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(int passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    //checks if 2 passengers are equals
+    @Override
+    public boolean equals(Object o) {
+        Passenger p = (Passenger) o;
+        if(passportNumber == p.passportNumber)
+            return true;
+        else return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", passport number: " + passportNumber;
+    }
+    
+}
---
+++ airline/project.java/pilot.java
@@ -0,0 +1,35 @@
+package airport.app;
+import java.util.Date;
+
+public class Pilot extends Person{
+    private String id;
+
+    public Pilot(String id, String name, Date dateOfBirth, String nationality) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    //checks if 2 pilot are equals
+    @Override
+    public boolean equals(Object o) {
+        Pilot p = (Pilot) o;
+        if(id == p.getId())
+            return true;
+        else return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Id: " + id;
+    }
+}
